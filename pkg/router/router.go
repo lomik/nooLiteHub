142e7b6a41bc1a49be3abb465255d9aeab733fdd
lomik <r.lomonosov@gmail.com>
2019-09-25 22:26:28 +0300

    router lookup

---
+++ pkg/router/router.go
@@ -0,0 +1,90 @@
+package router
+
+import "strings"
+
+type entry struct {
+	callback    func(ctx interface{})
+	staticChild map[string]*entry
+	paramChild  map[string]*entry
+}
+
+// Router ...
+type Router struct {
+	root   *entry
+	params map[string](func(value string, ctx interface{}) error)
+}
+
+// New creates new instance of Router
+func New() *Router {
+	return &Router{
+		root:   newEntry(),
+		params: make(map[string](func(value string, ctx interface{}) error)),
+	}
+}
+
+func newEntry() *entry {
+	return &entry{
+		staticChild: make(map[string]*entry),
+		paramChild:  make(map[string]*entry),
+	}
+}
+
+// AddPath ...
+func (r *Router) AddPath(path string, callback func(ctx interface{})) {
+	a := strings.Split(path, "/")
+	p := r.root
+	for _, w := range a {
+		if strings.HasPrefix(w, ":") {
+			n := p.paramChild[w[1:]]
+			if n == nil {
+				n = newEntry()
+				p.paramChild[w[1:]] = n
+			}
+			p = n
+		} else {
+			n := p.staticChild[w]
+			if n == nil {
+				n = newEntry()
+				p.staticChild[w] = n
+			}
+			p = n
+		}
+	}
+
+	p.callback = callback
+}
+
+// AddParam ...
+func (r *Router) AddParam(name string, parser func(value string, ctx interface{}) error) {
+	r.params[name] = parser
+}
+
+func (r *Router) lookup(root *entry, path []string) (*entry, map[string]string) {
+	// is leaf
+	if len(path) == 0 {
+		return root, make(map[string]string)
+	}
+
+	// check static
+	if c, exists := root.staticChild[path[0]]; exists {
+		e, m := r.lookup(c, path[1:])
+		if e != nil {
+			return e, m
+		}
+	}
+
+	for k, v := range root.paramChild {
+		e, m := r.lookup(v, path[1:])
+		if e != nil {
+			m[k] = path[0]
+			return e, m
+		}
+	}
+
+	return nil, nil
+}
+
+// Route ...
+func (r *Router) Route(path string, ctx interface{}) error {
+	return nil
+}
---
+++ pkg/router/router_test.go
@@ -0,0 +1,37 @@
+package router
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLookup(t *testing.T) {
+	assert := assert.New(t)
+	r := New()
+
+	addPath := func(path string) {
+		r.AddPath(path, func(ctx interface{}) { *(ctx.(*string)) = path })
+	}
+
+	test := func(path string, expectedRoute string, expectedParams map[string]string) {
+		e, m := r.lookup(r.root, strings.Split(path, "/"))
+		if expectedRoute == "" {
+			assert.Nil(e, path)
+			assert.Nil(m, path)
+		} else {
+			var p string
+			if assert.NotNil(e, path) {
+				e.callback(&p)
+				assert.Equal(expectedRoute, p, path)
+				assert.Equal(expectedParams, m, path)
+			}
+		}
+	}
+
+	addPath("rx/:ch/on")
+
+	test("rx/42/on", "rx/:ch/on", map[string]string{"ch": "42"})
+	test("rx/42/off", "", nil)
+}
